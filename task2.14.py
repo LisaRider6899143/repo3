9aaf815a455e773cde6d12a6e7a356627086f60c
LisaRider6899143 <124029343+LisaRider6899143@users.noreply.github.com>
2023-01-31 08:12:02 +0100

    Add files via upload

---
+++ task2.14.py
@@ -0,0 +1,6 @@
+n = int(input())
+seqSum = 0
+while n > 0:
+    seqSum += n**2
+    n -= 1
+print(seqSum)
---
+++ task2.15.py
@@ -0,0 +1,8 @@
+n = int(input())
+seqSum = 0
+i = 1
+while n != 0:
+    seqSum += n
+    i = i+1
+    n = int(input())
+print(i-1)
